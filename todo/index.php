1453d5f4e26bc24b2973f16a660259d2de7e0ff8
alice <amw46@njit.edu>
2018-05-09 14:51:04 -0400

    fixes to checks

--- todo/index.php
+++ todo/index.php
@@ -87,7 +87,7 @@ if ((!empty($_SESSION['auth']) || $_SESSION['auth'] == 'true')) {
     }
 
     else if ($action == "edit_todo") {
-        $tid = filter_input(INPUT_POST, "itemid");
+        $tid = filter_input(INPUT_GET, 'id');
         $todo = TodosDB::getTodoById($tid);
         //pasing variables along
 
